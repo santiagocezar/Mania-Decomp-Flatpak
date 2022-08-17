c8fab21826fdc76fce52ec74f8a2106b9593c16b
Santiago Cézar <santiagocezar2013@gmail.com>
2022-08-17 01:13:15 -0300

    find data.rsdk

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Automatically copy Data.rsdk from the installed game
-# Coming Soon™
+for data in "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/steamapps/common/Sonic Mania/Data.rsdk" "$HOME/.steam/steam/steamapps/common/Sonic Mania/Data.rsdk"
+do
+    if [ -f "$data" ]; then
+        cp "$data" .
+        break
+    fi
+done
 
 ln -srf /app/lib/Game.so .
 
